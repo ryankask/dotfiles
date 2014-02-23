278d05cfe5746b737358c4b153783bcb79bbb0bc
Ryan Kaskel <dev@ryankaskel.com>
2014-02-23 19:24:06 +0000

    Update profile.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,4 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-ancient "0.5.4"]
-                  [com.cemerick/austin "0.1.3"]]}}
+                  [com.cemerick/austin "0.1.3"]
+                  [lein-pprint "1.1.1"]]}}
