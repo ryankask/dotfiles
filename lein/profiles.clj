69bfeb653a73bcb98e8b3da98771ddbe18becc1d
Ryan Kaskel <dev@ryankaskel.com>
2014-10-20 08:13:05 +0100

    Use cider-nrepl snapshot.

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,4 +1,4 @@
 {:user {:plugins [[lein-kibit "0.0.8"]
                   [lein-ancient "0.5.5"]
                   [lein-pprint "1.1.2"]
-                  [cider/cider-nrepl "0.7.0"]]}}
+                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]]}}
