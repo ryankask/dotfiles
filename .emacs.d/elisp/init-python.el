5aadc55663c384a040258478c7e4358631fa8a05
Ryan Kaskel <dev@ryankaskel.com>
2015-01-19 11:03:10 +0000

    Temporarily disable Anaconda mode.

--- .emacs.d/elisp/init-python.el
+++ .emacs.d/elisp/init-python.el
@@ -8,24 +8,30 @@
   (interactive)
   (insert "import pudb; pu.db"))
 
+(defvar my-python-enable-anaconda-mode nil)
+
 (add-hook 'python-mode-hook
           (lambda ()
             (interactive)
             (column-marker-1 80)
             (column-marker-2 100)
-            (anaconda-mode)
-            (my-python-mode-set-company-backends)
+
             (define-key python-mode-map (kbd "C-m") 'newline-and-indent)
             (define-key python-mode-map (kbd "C-c /") 'python-debug-insert-ipdb-set-trace)
             (define-key python-mode-map (kbd "C-c p") 'python-debug-insert-pudb-set-trace)
-            (define-key anaconda-mode-map (kbd "s-.") 'anaconda-mode-goto-definitions)
-            (define-key anaconda-mode-map (kbd "s-*") 'anaconda-mode-nav-pop-marker)
-            (define-key anaconda-mode-map (kbd "s-?") 'anaconda-mode-view-doc)))
+
+            (when my-python-enable-anaconda-mode
+              (anaconda-mode)
+              (define-key anaconda-mode-map (kbd "s-.") 'anaconda-mode-goto-definitions)
+              (define-key anaconda-mode-map (kbd "s-*") 'anaconda-mode-nav-pop-marker)
+              (define-key anaconda-mode-map (kbd "s-?") 'anaconda-mode-view-doc))))
+
 
 (defun my-python-mode-set-company-backends ()
-  (set (make-local-variable 'company-backends)
-       '(company-capf
-         company-dabbrev-code
-         company-anaconda)))
+  (let ((my-company-backends) '(company-capf
+                                company-dabbrev-code))
+    (when my-python-enable-anaconda-mode
+      (append-to-list 'my-company-backends 'company-anaconda))
+    (set (make-local-variable 'company-backends) my-company-backends)))
 
 (provide 'init-python)
