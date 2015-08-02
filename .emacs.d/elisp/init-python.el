09db4d94c3a7aea367c091def5b55612e92cd776
Ryan Kaskel <dev@ryankaskel.com>
2015-08-02 18:14:44 +0100

    Reorganisee init-python

--- .emacs.d/elisp/init-python.el
+++ .emacs.d/elisp/init-python.el
@@ -7,14 +7,6 @@
       jedi:environment-root my-default-virtualenv-path
       flycheck-python-flake8-executable (f-join my-default-virtualenv-bin "flake8"))
 
-;; If `project-venv-name` is a string, mark it as safe
-(put 'project-venv-name 'safe-local-variable #'stringp)
-
-(defun python-debug-insert-ipdb-set-trace ()
-  "Insert ipdb trace call into buffer."
-  (interactive)
-  (insert "import ipdb; ipdb.set_trace()"))
-
 (add-hook 'python-mode-hook
           (lambda ()
             (column-marker-1 80)
@@ -22,10 +14,16 @@
             (define-key python-mode-map (kbd "C-m") 'newline-and-indent)
             (define-key python-mode-map (kbd "C-c /") 'python-debug-insert-ipdb-set-trace)
             (hack-local-variables)
-            (when (boundp 'project-venv-name)
-              (venv-workon project-venv-name))
             (jedi:setup)
             (my-python-mode-set-company-backends)))
+
+(defun python-debug-insert-ipdb-set-trace ()
+  "Insert ipdb trace call into buffer."
+  (interactive)
+  (insert "import ipdb; ipdb.set_trace()"))
+
+;; If `project-venv-name` is a string, mark it as safe
+(put 'project-venv-name 'safe-local-variable #'stringp)
 
 (defun my-python-mode-set-company-backends ()
   (set (make-local-variable 'company-backends)
