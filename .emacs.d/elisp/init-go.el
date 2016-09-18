102d1440b6d1fc814fa833bc423742fa35281088
Ryan Kaskel <dev@ryankaskel.com>
2016-09-18 13:13:17 +0100

    Tweak go config

--- .emacs.d/elisp/init-go.el
+++ .emacs.d/elisp/init-go.el
@@ -1,14 +1,18 @@
 (use-package go-mode
   :ensure t
-  :defer t
+  :bind (:map go-mode-map
+              ("C-c C-r" . go-remove-unused-imports)
+              ("s-h f" . godoc-at-point))
   :init
   (setq gofmt-show-errors nil)
   (add-hook 'go-mode-hook
             (lambda ()
-              (local-set-key (kbd "C-c C-r") 'go-remove-unused-imports)
-              (local-set-key (kbd "C-c C-g") 'go-goto-imports)
-              (local-set-key (kbd "C-c C-f") 'gofmt)
-              (local-set-key (kbd "C-c C-k") 'godoc)
+              ;; Prefer goreturns to gofmt if installed
+              (let ((goreturns (executable-find "goreturns")))
+                (when goreturns
+                  (setq gofmt-command goreturns)))
+
+              (subword-mode 1)
               (set (make-local-variable 'company-backends) '(company-go))
               (add-hook 'before-save-hook 'gofmt-before-save))))
 
