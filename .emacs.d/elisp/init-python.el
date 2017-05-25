c17844eee0eab326cefd2f6c011572d42f36018b
Ryan Kaskel <dev@ryankaskel.com>
2017-05-25 09:20:41 +0100

    Automatically activate virtualenv

--- .emacs.d/elisp/init-python.el
+++ .emacs.d/elisp/init-python.el
@@ -1,3 +1,14 @@
+(defun my-python-activate-virtualenv ()
+  "Find and activate the project's virtualenv root directory"
+  (let* ((project-root (projectile-project-root))
+         (virtualenv-root-file (concat project-root ".virtualenv-root")))
+    (ignore-errors
+      (with-temp-buffer
+        (insert-file-contents virtualenv-root-file)
+        (let ((virtualenv-root (string-trim (thing-at-point 'line t))))
+          (if (not (string-empty-p virtualenv-root))
+              (setq python-shell-virtualenv-root virtualenv-root)))))))
+
 (defun my-python-debug-insert-ipdb-set-trace ()
   "Insert ipdb trace call into buffer."
   (interactive)
@@ -18,9 +29,9 @@
   (setq my-default-virtualenv-path (python-environment-root-path)
         python-shell-virtualenv-root my-default-virtualenv-path
         flycheck-python-flake8-executable (python-environment-bin "flake8" my-default-virtualenv-path))
-  (put 'python-shell-virtualenv-root 'safe-local-variable #'stringp)
   (add-hook 'python-mode-hook
             (lambda ()
+              (my-python-activate-virtualenv)
               (column-marker-1 80)
               (column-marker-2 100))))
 
