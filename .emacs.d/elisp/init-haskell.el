6677f16e8a625514727f3d7a46352e6b04599c7d
Ryan Kaskel <dev@ryankaskel.com>
2017-11-04 08:30:23 +0000

    Update Haskell configuration

--- .emacs.d/elisp/init-haskell.el
+++ .emacs.d/elisp/init-haskell.el
@@ -1,6 +1,7 @@
 (use-package haskell-mode
   :ensure t
   :pin melpa-stable
+  :defer t
   :init
   (add-hook 'haskell-mode-hook
             (lambda ()
@@ -8,5 +9,17 @@
               (set (make-local-variable 'company-backends)
                    (append '((company-capf company-dabbrev-code))
                            company-backends)))))
+
+(use-package intero
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'haskell-mode-hook 'intero-mode))
+
+(use-package hindent
+  :ensure t
+  :defer t
+  :init
+  (add-hook 'haskell-mode-hook 'hindent-mode))
 
 (provide 'init-haskell)
