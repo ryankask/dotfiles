50ba894e1a6106dc527915bf2b2d3c5272b47e53
Ryan Kaskel <dev@ryankaskel.com>
2020-01-10 11:38:25 +0000

    Stop formatting on save for all modes

--- .emacs.d/elisp/init-go.el
+++ .emacs.d/elisp/init-go.el
@@ -1,8 +1,13 @@
 ;;; -*- lexical-binding: t; -*-
 
+(defun my-go-mode-before-save ()
+  (lsp-format-buffer)
+  (lsp-organize-imports))
+
 (defun my-go-mode-setup ()
   "Hook to run when go-mode is enabled"
   (subword-mode 1)
+  (add-hook 'before-save-hook #'my-go-mode-before-save)
   (lsp-deferred))
 
 (use-package go-mode
@@ -10,9 +15,6 @@
   :bind (:map go-mode-map
               ("C-c C-r" . go-remove-unused-imports)
               ("s-h f" . godoc-at-point))
-  :hook ((go-mode . my-go-mode-setup)
-         (before-save . lsp-format-buffer)
-         (before-save . lsp-organize-imports)))
-
+  :hook ((go-mode . my-go-mode-setup)))
 
 (provide 'init-go)
