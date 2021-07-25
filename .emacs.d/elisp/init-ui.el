20761365dad58198681ef5818f54a87356818917
Ryan Kaskel <dev@ryankaskel.com>
2021-07-25 13:12:40 +0100

    Update theme

--- .emacs.d/elisp/init-ui.el
+++ .emacs.d/elisp/init-ui.el
@@ -43,38 +43,22 @@
       '(read-only t cursor-intangible t face minibuffer-prompt))
 (add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
 
+(defun my-modus-themes-custom-faces ())
 
-(defun my-doom-one-theme-customise ()
-  (interactive)
-  (doom-themes-set-faces
-   'user
-   ;; base
-   '(cursor :background fg)
-   '(minibuffer-prompt :foreground fg)
-   '(font-lock-variable-name-face :foreground fg)
-   '(line-number-current-line :foreground base4 :background bg)
-   ;; dired
-   '(dired-directory :foreground blue)
-   ;; avy
-   '(avy-goto-char-timer-face :background blue :foreground base0)
-   '(avy-lead-face :background yellow :foreground base0)
-   '(avy-lead-face-0 :background red :foreground base0)
-   '(avy-lead-face-1 :background orange :foreground base0)
-   '(avy-lead-face-2 :background violet :foreground base0)
-   ;; column-marker
-   '(column-marker-1 :background dark-blue)
-   '(column-marker-2 :background blue)
-   ;; ivy
-   ;; org
-   '(org-level-1 :foreground blue :background base3 :bold t :height 1.0)
-   ;; lsp-ui
-   '(lsp-ui-doc-background :background bg-alt)
-   '(lsp-ui-doc-header :background highlight :foreground bg)))
-
-(use-package doom-themes
+(use-package modus-themes
   :straight t
+  :custom
+  (modus-themes-bold-constructs nil)
+  (modus-themes-italic-constructs t)
+  (modus-themes-mode-line '(3d))
+  (modus-themes-region '(bg-only))
+  (modus-themes-paren-match '(intense))
+  (modus-themes-completions 'opinionated)
+  :bind ("C-o w" . modus-themes-toggle)
   :init
-  (load-theme 'doom-one t)
-  (my-doom-one-theme-customise))
+  (add-hook 'modus-themes-after-load-theme-hook #'my-modus-themes-custom-faces)
+  (modus-themes-load-themes)
+  :config
+  (modus-themes-load-vivendi))
 
 (provide 'init-ui)
