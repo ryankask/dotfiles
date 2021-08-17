d2ab17c2b398fa685472214264aeeb72acb84af5
Ryan Kaskel <dev@ryankaskel.com>
2021-08-17 10:22:56 +0200

    Use consult for in region completion

--- .emacs.d/elisp/init-completion.el
+++ .emacs.d/elisp/init-completion.el
@@ -95,7 +95,14 @@
   (setq consult-project-root-function
         (lambda ()
           (when-let (project (project-current))
-            (car (project-roots project))))))
+            (car (project-roots project)))))
+
+  (setq completion-in-region-function
+        (lambda (&rest args)
+          (apply (if vertico-mode
+                     #'consult-completion-in-region
+                   #'completion--in-region)
+                 args))))
 
 (use-package marginalia
   :straight t
