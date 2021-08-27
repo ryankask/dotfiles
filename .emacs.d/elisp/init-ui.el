e9f29ec16aac8e09494954d5847c8a679de56ba3
Ryan Kaskel <dev@ryankaskel.com>
2021-08-28 00:13:58 +0200

    Add title bar style

--- .emacs.d/elisp/init-ui.el
+++ .emacs.d/elisp/init-ui.el
@@ -31,9 +31,36 @@
       minibuffer-prompt-properties
       '(read-only t cursor-intangible t face minibuffer-prompt))
 
-(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
+(defvar my-theme-frame-settings nil
+  "Alist that contains frame settings specific to the current theme")
 
-(defun my-modus-themes-custom-faces ())
+(defun my-theme-get-ns-frame-parameters ()
+  `((ns-transparent-titlebar . t)
+    (ns-appearance . ,(alist-get 'background-mode my-theme-frame-settings))))
+
+(defun my-theme-update-frame-defaults ()
+  (pcase-dolist (`(,property . ,value) (my-theme-get-ns-frame-parameters))
+    (setf (alist-get property default-frame-alist) value)))
+
+(defun my-theme-update-frame-titlebar (frame &rest _)
+  "Based on https://github.com/purcell/ns-auto-titlebar/blob/master/ns-auto-titlebar.el"
+  (when-let (display-graphic-p frame)
+    (modify-frame-parameters frame (my-theme-get-ns-frame-parameters))))
+
+(defun my-theme-update-all-frame-titlebars ()
+  (mapc #'my-theme-update-frame-titlebar (frame-list)))
+
+(defun my-theme-configure-frames ()
+  (my-theme-update-all-frame-titlebars)
+  (my-theme-update-frame-defaults))
+
+(defun my-modus-themes-setup ()
+  (setf (alist-get 'background-mode my-theme-frame-settings)
+        (pcase (modus-themes--current-theme)
+          ('modus-operandi 'light)
+          ('modus-vivendi 'dark)
+          (theme (error "'%s' is not a Modus theme" theme))))
+  (my-theme-configure-frames))
 
 (use-package modus-themes
   :straight t
@@ -46,7 +73,7 @@
   (modus-themes-completions 'opinionated)
   :bind ("C-o w" . modus-themes-toggle)
   :init
-  (add-hook 'modus-themes-after-load-theme-hook #'my-modus-themes-custom-faces)
+  (add-hook 'modus-themes-after-load-theme-hook #'my-modus-themes-setup)
   (modus-themes-load-themes)
   (modus-themes-load-vivendi))
 
