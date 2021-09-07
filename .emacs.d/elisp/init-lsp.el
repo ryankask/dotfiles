2b0b11add50452ac9050c0a4fa0d7f3cd3a523c5
Ryan Kaskel <dev@ryankaskel.com>
2021-09-07 10:11:08 +0200

    Remove underlines from help buffer

--- .emacs.d/elisp/init-lsp.el
+++ .emacs.d/elisp/init-lsp.el
@@ -4,13 +4,19 @@
   ;; Optimisations - copied from Doom Emacs
   (setq-local gcmh-high-cons-threshold (* 2 (default-value 'gcmh-high-cons-threshold))))
 
+(defun my-lsp-help-mode-setup ()
+  "Customize faces for the lsp-help buffer"
+  (when (string= (buffer-name) "*lsp-help*")
+    (face-remap-add-relative 'nobreak-space :underline nil)))
+
 (use-package lsp-mode
   :straight t
   :commands (lsp lsp-deferred)
   :bind (:map lsp-mode-map
               ("C-o d" . lsp-describe-thing-at-point)
               ("C-o f" . lsp-format-buffer))
-  :hook (lsp-mode . my-lsp-mode-setup)
+  :hook ((lsp-mode . my-lsp-mode-setup)
+         (help-mode . my-lsp-help-mode-setup))
   :custom
   (lsp-enable-snippet nil)
   (lsp-headerline-breadcrumb-enable nil)
