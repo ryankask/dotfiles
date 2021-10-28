817347d4fdd65df0815729ccfad9b1741ac88129
Ryan Kaskel <dev@ryankaskel.com>
2021-10-28 09:25:40 +0100

    Fix which key indicator

--- .emacs.d/elisp/init-completion.el
+++ .emacs.d/elisp/init-completion.el
@@ -139,16 +139,29 @@ current target followed by an ellipsis if there are further
 targets."
   (lambda (&optional keymap targets prefix)
     (if (null keymap)
-        (kill-buffer which-key--buffer)
+        (which-key--hide-popup-ignore-command)
       (which-key--show-keymap
-       (if (eq (caar targets) 'embark-become)
+       (if (eq (plist-get (car targets) :type) 'embark-become)
            "Become"
          (format "Act on %s '%s'%s"
                  (plist-get (car targets) :type)
                  (embark--truncate-target (plist-get (car targets) :target))
                  (if (cdr targets) "…" "")))
-       (if prefix (lookup-key keymap prefix) keymap)
-       nil nil t))))
+       (if prefix
+           (pcase (lookup-key keymap prefix 'accept-default)
+             ((and (pred keymapp) km) km)
+             (_ (key-binding prefix 'accept-default)))
+         keymap)
+       nil nil t (lambda (binding)
+                   (not (string-suffix-p "-argument" (cdr binding))))))))
+
+(defun embark-hide-which-key-indicator (fn &rest args)
+  "Hide the which-key indicator immediately when using the completing-read prompter."
+  (when-let ((win (get-buffer-window which-key--buffer
+                                     'visible)))
+    (quit-window 'kill-buffer win)
+    (let ((embark-indicators (delq #'embark-which-key-indicator embark-indicators)))
+      (apply fn args))))
 
 (defun my-open-file-in-finder (file)
   "Reveal FILE in finder. If FILE is a directory, open it directly in Finder so its contents are displayed instead of revealing it."
@@ -166,9 +179,12 @@ targets."
          ("s-." . embark-dwim)
          :map embark-file-map
          ("X" . my-open-file-in-finder))
+  :custom (embark-indicators '(embark-which-key-indicator
+                               embark-highlight-indicator
+                               embark-isearch-highlight-indicator))
   :init
-  (setq prefix-help-command #'embark-prefix-help-command)
-  (advice-add #'embark-verbose-indicator :override #'embark-which-key-indicator)
+  (advice-add #'embark-completing-read-prompter
+              :around #'embark-hide-which-key-indicator)
   :config
   (add-to-list 'display-buffer-alist
                '("\\`\\*Embark Collect \\(Live\\|Completions\\)\\*"
