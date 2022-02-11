da8e9261612ee76bea23690f6778efd96014c94e
Ryan Kaskel <dev@ryankaskel.com>
2022-02-11 16:41:52 +0000

    Back out of new settings

--- .emacs.d/elisp/init-web.el
+++ .emacs.d/elisp/init-web.el
@@ -23,26 +23,4 @@
     (sp-local-tag "=" "<%= " " %>")
     (sp-local-tag "#" "<%# " " %>")))
 
-(use-package typescript-mode
-  :straight t
-  :init
-  (define-derived-mode typescript-tsx-mode typescript-mode "tsx")
-  :config
-  (setq typescript-indent-level 2)
-  (add-hook 'typescript-mode #'subword-mode)
-  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescript-tsx-mode)))
-
-(use-package tree-sitter
-  :straight t
-  :hook ((typescript-mode . tree-sitter-hl-mode)
-         (typescript-tsx-mode . tree-sitter-hl-mode)))
-
-(use-package tree-sitter-langs
-  :straight t
-  :after tree-sitter
-  :config
-  (tree-sitter-require 'tsx)
-  (add-to-list 'tree-sitter-major-mode-language-alist
-               '(typescript-tsx-mode . tsx)))
-
 (provide 'init-web)
