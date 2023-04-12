d6c89f98274fc010797f18da6006d6dd9d73a7f0
Ryan Kaskel <dev@ryankaskel.com>
2023-04-12 11:16:57 +0100

    Copy additional env var

--- .emacs.d/elisp/init-macos.el
+++ .emacs.d/elisp/init-macos.el
@@ -9,7 +9,8 @@
   :straight t
   :config
   (setq exec-path-from-shell-arguments nil
-        exec-path-from-shell-variables (append exec-path-from-shell-variables '("GOPATH")))
+        exec-path-from-shell-variables (append exec-path-from-shell-variables
+                                               (list "GOPATH" "DOTFILES")))
   (exec-path-from-shell-initialize))
 
 ;; Disable `ns-popup-font-panel', which causes emacs to sometimes freeze
