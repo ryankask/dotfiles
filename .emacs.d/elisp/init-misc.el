229447ed67db15007ec0fc6ba2f9caba1cb32e0a
Ryan Kaskel <dev@ryankaskel.com>
2024-01-25 09:24:14 +0000

    Add Anyscale

--- .emacs.d/elisp/init-misc.el
+++ .emacs.d/elisp/init-misc.el
@@ -150,12 +150,19 @@ from a GCP release notes entry."
   :bind (("C-c #" . gptel)
          ("C-c RET" . gptel-send))
   :config
-  (gptel-make-openai
-   "Mistral"
-   :header (lambda () `(("Authorization" . ,(concat "Bearer " (gptel--get-api-key)))))
-   :host "api.mistral.ai"
-   :stream t
-   :models '("mistral-medium" "mistral-small" "mistral-tiny")))
+  (gptel-make-openai "Mistral"
+    :host "api.mistral.ai"
+    :key 'gptel-api-key
+    :stream t
+    :models '("mistral-medium" "mistral-small" "mistral-tiny"))
+
+  (gptel-make-openai "Anyscale"
+    :host "api.endpoints.anyscale.com"
+    :key 'gptel-api-key
+    :models '("meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-chat-hf"
+              "meta-llama/Llama-2-70b-chat-hf" "codellama/CodeLlama-34b-Instruct-hf"
+              "mistralai/Mistral-7B-Instruct-v0.1" "mistralai/Mixtral-8x7B-Instruct-v0.1"
+              "HuggingFaceH4/zephyr-7b-beta" "Open-Orca/Mistral-7B-OpenOrca")))
 
 ;; internal
 (use-package hcl-ts-mode
