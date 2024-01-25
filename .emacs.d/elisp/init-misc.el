b9af070becbf84c7d46026b996e2174d09c5eb5b
Ryan Kaskel <dev@ryankaskel.com>
2024-01-25 09:27:16 +0000

    Stream Anyscale responses

--- .emacs.d/elisp/init-misc.el
+++ .emacs.d/elisp/init-misc.el
@@ -159,6 +159,7 @@ from a GCP release notes entry."
   (gptel-make-openai "Anyscale"
     :host "api.endpoints.anyscale.com"
     :key 'gptel-api-key
+    :stream t
     :models '("meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-chat-hf"
               "meta-llama/Llama-2-70b-chat-hf" "codellama/CodeLlama-34b-Instruct-hf"
               "mistralai/Mistral-7B-Instruct-v0.1" "mistralai/Mixtral-8x7B-Instruct-v0.1"
