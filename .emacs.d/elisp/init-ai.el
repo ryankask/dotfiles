ff727099017833a3a1e4db6e9fd3be1e596e4740
Ryan Kaskel <dev@ryankaskel.com>
2024-02-27 22:19:43 +0000

    Update Anyscale models

--- .emacs.d/elisp/init-ai.el
+++ .emacs.d/elisp/init-ai.el
@@ -17,9 +17,19 @@
     :host "api.endpoints.anyscale.com"
     :key 'gptel-api-key
     :stream t
-    :models '("meta-llama/Llama-2-7b-chat-hf" "meta-llama/Llama-2-13b-chat-hf"
-              "meta-llama/Llama-2-70b-chat-hf" "codellama/CodeLlama-34b-Instruct-hf"
-              "mistralai/Mistral-7B-Instruct-v0.1" "mistralai/Mixtral-8x7B-Instruct-v0.1"
-              "HuggingFaceH4/zephyr-7b-beta" "Open-Orca/Mistral-7B-OpenOrca")))
+    :models '("meta-llama/Llama-2-7b-chat-hf"
+              "mistralai/Mistral-7B-Instruct-v0.1"
+              "meta-llama/Llama-2-13b-chat-hf"
+              "meta-llama/Llama-2-70b-chat-hf"
+              "mistralai/Mixtral-8x7B-Instruct-v0.1"
+              "BAAI/bge-large-en-v1.5"
+              "thenlper/gte-large"
+              "mlabonne/NeuralHermes-2.5-Mistral-7B"
+              "Meta-Llama/Llama-Guard-7b"
+              "codellama/CodeLlama-70b-Instruct-hf"
+              "Open-Orca/Mistral-7B-OpenOrca"
+              "codellama/CodeLlama-34b-Instruct-hf"
+              "HuggingFaceH4/zephyr-7b-beta"
+              "google/gemma-7b-it")))
 
 (provide 'init-ai)
