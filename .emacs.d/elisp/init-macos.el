60114875421c4d2643a556d4d93508d94e82bd5b
Ryan Kaskel <dev@ryankaskel.com>
2024-04-24 07:59:28 +0100

    Remove (elpaca nil ...)

--- .emacs.d/elisp/init-macos.el
+++ .emacs.d/elisp/init-macos.el
@@ -27,9 +27,10 @@
   (when-let ((word (word-at-point t)))
     (my-dict-app-lookup-word word)))
 
-(elpaca nil
-  (with-eval-after-load 'embark
-    (bind-key "f" #'my-dict-app-lookup-word-at-point embark-identifier-map)))
+(add-hook
+ 'elpaca-after-init-hook
+ (lambda ()
+   (bind-key "f" #'my-dict-app-lookup-word-at-point embark-identifier-map)))
 
 ;; Rectangle app helpers
 
@@ -71,41 +72,42 @@
       (my-rectangle-exec-action action))
     (format "Execute Rectangle app action `%s'" action)))
 
-(elpaca nil
-  (with-eval-after-load 'transient
-    (transient-define-prefix my-rectangle-dispatch ()
-      "Rectangle app dispatcher"
-      [["Halves"
-        ("l" "left half" my-rectangle-exec-action--left-half)
-        ("r" "right half" my-rectangle-exec-action--right-half)
-        ("c" "center half" my-rectangle-exec-action--center-half)
-        ("th" "top half" my-rectangle-exec-action--top-half)
-        ("bh" "bottom half" my-rectangle-exec-action--bottom-half)]
-       ["Corners"
-        ("tl" "top left" my-rectangle-exec-action--top-left)
-        ("tr" "top right" my-rectangle-exec-action--top-right)
-        ("bl" "bottom left" my-rectangle-exec-action--bottom-left)
-        ("br" "bottom right" my-rectangle-exec-action--bottom-right)]
-       ["Thirds"
-        ("3l" "first third" my-rectangle-exec-action--first-third)
-        ("3c" "center third" my-rectangle-exec-action--center-third)
-        ("3r" "last third" my-rectangle-exec-action--last-third)
-        ("," "first two thirds" my-rectangle-exec-action--first-two-thirds)
-        ("." "last two thirds" my-rectangle-exec-action--last-two-thirds)]
-       ["Edges"
-        ("el" "move left" my-rectangle-exec-action--move-left)
-        ("er" "move right" my-rectangle-exec-action--move-right)
-        ("eu" "move up" my-rectangle-exec-action--move-up)
-        ("ed" "move down" my-rectangle-exec-action--move-down)]
-       ["Size"
-        ("m" "maximise" my-rectangle-exec-action--maximize)
-        ("a" "almost maximise" my-rectangle-exec-action--almost-maximize)
-        ("u" "maximise height" my-rectangle-exec-action--maximize-height)
-        ("-" "smaller" my-rectangle-exec-action--smaller)
-        ("+" "larger" my-rectangle-exec-action--larger)
-        ("=" "center" my-rectangle-exec-action--center)
-        ("v" "restore" my-rectangle-exec-action--restore)]])
+(defun my-rectangle-set-up-transient ()
+  "Set up the rectangle transient"
+  (transient-define-prefix my-rectangle-dispatch ()
+    "Rectangle app dispatcher"
+    [["Halves"
+      ("l" "left half" my-rectangle-exec-action--left-half)
+      ("r" "right half" my-rectangle-exec-action--right-half)
+      ("c" "center half" my-rectangle-exec-action--center-half)
+      ("th" "top half" my-rectangle-exec-action--top-half)
+      ("bh" "bottom half" my-rectangle-exec-action--bottom-half)]
+     ["Corners"
+      ("tl" "top left" my-rectangle-exec-action--top-left)
+      ("tr" "top right" my-rectangle-exec-action--top-right)
+      ("bl" "bottom left" my-rectangle-exec-action--bottom-left)
+      ("br" "bottom right" my-rectangle-exec-action--bottom-right)]
+     ["Thirds"
+      ("3l" "first third" my-rectangle-exec-action--first-third)
+      ("3c" "center third" my-rectangle-exec-action--center-third)
+      ("3r" "last third" my-rectangle-exec-action--last-third)
+      ("," "first two thirds" my-rectangle-exec-action--first-two-thirds)
+      ("." "last two thirds" my-rectangle-exec-action--last-two-thirds)]
+     ["Edges"
+      ("el" "move left" my-rectangle-exec-action--move-left)
+      ("er" "move right" my-rectangle-exec-action--move-right)
+      ("eu" "move up" my-rectangle-exec-action--move-up)
+      ("ed" "move down" my-rectangle-exec-action--move-down)]
+     ["Size"
+      ("m" "maximise" my-rectangle-exec-action--maximize)
+      ("a" "almost maximise" my-rectangle-exec-action--almost-maximize)
+      ("u" "maximise height" my-rectangle-exec-action--maximize-height)
+      ("-" "smaller" my-rectangle-exec-action--smaller)
+      ("+" "larger" my-rectangle-exec-action--larger)
+      ("=" "center" my-rectangle-exec-action--center)
+      ("v" "restore" my-rectangle-exec-action--restore)]])
+  (bind-key "s-f" #'my-rectangle-dispatch))
 
-    (bind-key "s-f" #'my-rectangle-dispatch)))
+(add-hook 'elpaca-after-init-hook #'my-rectangle-set-up-transient)
 
 (provide 'init-macos)
