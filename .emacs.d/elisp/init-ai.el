ed1cb6880f6b5aac5864d3e54083464948a1ceee
Ryan Kaskel <dev@ryankaskel.com>
2024-09-20 08:55:57 +0100

    Remove Anyscale

--- .emacs.d/elisp/init-ai.el
+++ .emacs.d/elisp/init-ai.el
@@ -17,21 +17,6 @@
               "mistral-tiny"
               "codestral-latest"))
 
-  (gptel-make-openai "Anyscale"
-    :host "api.endpoints.anyscale.com"
-    :key 'gptel-api-key
-    :stream t
-    :models '("mistralai/Mistral-7B-Instruct-v0.1"
-              "meta-llama/Meta-Llama-3-70B-Instruct"
-              "mistralai/Mixtral-8x7B-Instruct-v0.1"
-              "meta-llama/Meta-Llama-3-8B-Instruct"
-              "BAAI/bge-large-en-v1.5"
-              "thenlper/gte-large"
-              "mlabonne/NeuralHermes-2.5-Mistral-7B"
-              "mistralai/Mixtral-8x22B-Instruct-v0.1"
-              "google/gemma-7b-it"
-              "llava-hf/llava-v1.6-mistral-7b-hf"))
-
   (gptel-make-anthropic "Claude"
     :stream t
     :key 'gptel-api-key)
