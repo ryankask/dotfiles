c28ed72e77eba252593605b106d34a83b35f9807
Ryan Kaskel <dev@ryankaskel.com>
2025-02-19 08:12:03 +0000

    Update Prolog setup

--- .emacs.d/elisp/init-logic.el
+++ .emacs.d/elisp/init-logic.el
@@ -1,10 +1,15 @@
 ;;; -*- lexical-binding: t; -*-
 
+(use-package prolog
+  :defer t
+  :custom
+  (prolog-indent-width 2))
+
 (use-package ediprolog
   :ensure t
   :after prolog
   :bind (nil
          :map prolog-mode-map
-         ("s-e" . ediprolog-dwim)))
+         ("C-c C-e" . ediprolog-dwim)))
 
 (provide 'init-logic)
