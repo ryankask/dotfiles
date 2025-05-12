88853b51b4f26b6140b7f46215da669c2ec9d55b
Ryan Kaskel <dev@ryankaskel.com>
2025-05-12 11:58:28 +0100

    Change shells

--- .emacs.d/elisp/init-misc.el
+++ .emacs.d/elisp/init-misc.el
@@ -545,8 +545,6 @@ session."
   (vterm-max-scrollback 5000)
   (vterm-timer-delay 0.025)
   (vterm-toggle-scope 'project)
-  (vterm-tramp-shells '(("ssh" "/usr/bin/zsh")
-                        ("docker" "/bin/sh")))
   :bind (nil
          :map vterm-mode-map
          ("C-k" . my-vterm-send-C-k)
