162f1de29ba43502bdac614c9e4f74ffc0bd0a45
Ryan Kaskel <dev@ryankaskel.com>
2025-07-08 09:19:37 +0100

    Try load path optimisation

--- .emacs.d/early-init.el
+++ .emacs.d/early-init.el
@@ -15,6 +15,10 @@
       jka-compr-verbose init-file-debug
       package-enable-at-startup nil)
 
+;; My additions
+
+(setq load-path-filter-function #'load-path-filter-cache-directory-files)
+
 ;;  Silence obsoletion warnings about (if|when)-let in >=31.
 (put 'if-let 'byte-obsolete-info nil)
 (put 'when-let 'byte-obsolete-info nil)
