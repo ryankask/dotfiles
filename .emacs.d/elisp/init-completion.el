f606649d9ed7ec13a4b594e45260479e0f97ab83
Ryan Kaskel <dev@ryankaskel.com>
2026-01-15 06:47:29 +0000

    Use public consult variables

--- .emacs.d/elisp/init-completion.el
+++ .emacs.d/elisp/init-completion.el
@@ -132,8 +132,8 @@
    :preview-key '(:debounce 0.2 any)
    consult-ripgrep consult-git-grep consult-grep
    consult-bookmark consult-recent-file consult-xref
-   consult--source-recent-file consult--source-project-recent-file
-   consult--source-bookmark
+   consult-source-recent-file consult-source-project-recent-file
+   consult-source-bookmark
    :preview-key "M-.")
   (add-to-list 'consult-buffer-sources 'my-consult-terminal-source 'append))
 
